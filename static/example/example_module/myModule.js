2521c965f013c8f1ddfea39f5b0f7d615aa63f9a
Maya <jaffe.maya@gmail.com>
2019-06-21 12:52:48 -0600

    add babel and see if this will allow it to work?

--- static/example/example_module/myModule.js
+++ static/example/example_module/myModule.js
@@ -1,7 +1,5 @@
-class Analysis {
+export default class Analysis {
 	constructor () { this.value = 0 }
 	increment () { this.value += 1; return this }
 	display () { return this.value }
 }
-
-export { Analysis }
